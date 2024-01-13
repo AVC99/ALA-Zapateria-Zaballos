22f4a3b4d658e78c2c462f5b2062c8d76f65b477
Alex Luelmo <alexitoluelmito@gmail.com>
2024-01-13 17:47:35 +0100

    Add Node class and refactor Branch and Bound

---
+++ model/Node.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, index, part1, part2, price_part1, price_part2, difference):
+        self.index = index
+        self.part1 = part1
+        self.part2 = part2
+        self.price_part1 = price_part1
+        self.price_part2 = price_part2
+        self.difference = difference
